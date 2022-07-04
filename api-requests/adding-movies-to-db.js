39b39c29d779df07a67e573f075a7fa5d195f48c
Hamza Ashraf <ashrah4@mcmaster.ca>
2022-07-04 17:18:45 -0400

    new api integrated and db populated

--- api-requests/adding-movies-to-db.js
+++ api-requests/adding-movies-to-db.js
@@ -1,55 +1,57 @@
 // Request to obtain complete movie directory from IMDB (just to be made once, and then, you can just update daily)
-// Each page contains 50 titles, run the query for pages 1-40 to obtain movies
-
+// There are 26 genres in total. Aim to obtain 1000 movies for each genre (without currently accounting for duplicates).
+// Make an API request to retrieve the top 250 movies pertaining to one genre, and repeat 8 times (1-250)(251-500)(501-750)(751-1000) and so on.
+// Do this for each of the genres and keep storing to the database.
+// PROGRESS
+// Action (1-1000) Adventure (1-1000) Animation () Biography () Comedy () Crime () Documentary () Drama () Family ()
+// Fantasy () Film-Noir () Game-Show () History () Horror () Music () Musical () Mystery () News () Reality_TV ()
+// Romance () Sci_Fi () Sport () Talk_Show () Thriller () War () Western ()
+require("dotenv").config();
+const api_key = process.env.API_KEY;
 const axios = require("axios");
 const movieModel = require("../models/movies-model.js");
-const options = require("./imdbrequest.js");
 require("../models/db-connection.js");
 
-const addMovieDetailsToDb = (response) => {
-  const results = response.data.results;
-  results.forEach((movie) => {
-    const newModel = new movieModel({
-      imdbid: movie.imdbid,
-      imageurl: movie.imageurl[0],
-      title: movie.title,
-      genre: movie.genre,
-      type: "Movie",
-      imdbrating: movie.imdbrating,
-      synopsis: movie.synopsis,
-      releasedate: movie.released,
-    });
-    newModel.save((err, result) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        // console.log(result);
-      }
-    });
-  });
+const options = {
+  method: "GET",
+  url: `https://imdb-api.com/API/AdvancedSearch/${api_key}?title_type=feature,tv_movie,short&genres=adventure&languages=en&moviemeter=751,1000&count=250`,
+  headers: {},
 };
 
 const getMovieDetailsFromIMDB = () => {
   axios
     .request(options)
     .then(function (response) {
+      console.log(response.data.results.length);
       addMovieDetailsToDb(response);
     })
     .catch(function (error) {
-      // console.error(error);
+      console.error(error);
     });
 };
 
-getMovieDetailsFromIMDB();
+const addMovieDetailsToDb = (response) => {
+  const results = response.data.results;
+  results.forEach((movie) => {
+    const newModel = new movieModel({
+      imdbid: movie.id,
+      image: movie.image,
+      title: movie.title,
+      runtimeStr: movie.runtimeStr,
+      genres: movie.genres,
+      imDbRating: movie.imDbRating,
+      plot: movie.plot,
+      stars: movie.stars,
+      releasedate: movie.description,
+    });
+    newModel.save((err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
+    });
+  });
+};
 
-/*
-How will the movie entries be stored in the db
-- imdb code
-- title
-- type
-- genre [array]
-- imdb rating
-- release date
-- synopsis
-- image url
-*/
+getMovieDetailsFromIMDB();
