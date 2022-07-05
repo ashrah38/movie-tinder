3635c99f6f634f83d7e34cd4b2925174ead3431d
Hamza Ashraf <ashrah4@mcmaster.ca>
2022-07-05 17:11:38 -0400

    endpoint updated for retrieval of movies from db matching upto 3 genres

--- routes/retrieving-movies.js
+++ routes/retrieving-movies.js
@@ -1,14 +1,25 @@
 const express = require("Express");
 const movieModel = require("../models/movies-model.js");
 const router = express.Router();
+// users can pick 3 genres at the choose category screen
+// then, movies fitting 2 of the 3 genres will be retrieved from the database
+// each user will have a list of movies they have already viewed - imdbid will be cross-checked to ensure that there are no duplicates.
 
-// can pick up to 5 genres
 router.get("/", (req, res) => {
   if (req.body) {
     const genres = req.body.genres;
+    const alreadyWatched = req.body.imdbids;
   }
+  // Testing purposes
+  const genreOne = "Action";
+  const genreTwo = "Adventure";
+  const genreThree = "Comedy";
   movieModel
-    .find({ genre: { $all: genres } })
+    .find({
+      genres: { $regex: genreOne, $options: "i" },
+      genres: { $regex: genreTwo, $options: "i" },
+      genres: { $regex: genreThree, $options: " i" },
+    })
     .then((res) => res.send(JSON.stringify(res)));
 });
 
