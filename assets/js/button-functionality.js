a9a3d804d68ed6dd4ba5e45aae13542a34c2fdfc
Hamza Ashraf <ashrah4@mcmaster.ca>
2022-07-12 02:31:49 -0400

    Buttons along with functionality added.

--- assets/js/button-functionality.js
+++ assets/js/button-functionality.js
@@ -1,7 +1,12 @@
+import { trackMovies } from "./track-likes-dislikes.js";
+
 export const addButtonFunctionality = () => {
-  const moreInfoOverlays = document.querySelectorAll(".more-info");
   const moreInfoBtns = document.querySelectorAll(".more-info-btn");
   const closeInfoBtns = document.querySelectorAll(".close-info-btn");
+  const likeBtn = document.querySelector(".like-btn");
+  const dislikeBtn = document.querySelector(".dislike-btn");
+  const cardDeck = document.querySelector(".card-stack");
+
   moreInfoBtns.forEach((moreInfoBtn) => {
     moreInfoBtn.addEventListener("click", (event) => {
       event.target.nextElementSibling.style.display = "inline";
@@ -12,5 +17,24 @@ export const addButtonFunctionality = () => {
     closeInfoBtn.addEventListener("click", (event) => {
       event.target.parentElement.parentElement.style.display = "none";
     });
+  });
+
+  likeBtn.addEventListener("click", () => {
+    let topCard = cardDeck.lastChild;
+    let movieId = topCard.id;
+    topCard.style.transform = "translate(3000px) rotate(30deg)";
+    trackMovies(movieId, "like");
+    setTimeout(() => {
+      cardDeck.removeChild(document.getElementById(movieId));
+    }, 200);
+  });
+  dislikeBtn.addEventListener("click", () => {
+    let topCard = cardDeck.lastChild;
+    let movieId = topCard.id;
+    topCard.style.transform = "translate(-3000px) rotate(-30deg)";
+    trackMovies(movieId, "dislike");
+    setTimeout(() => {
+      cardDeck.removeChild(document.getElementById(movieId));
+    }, 100);
   });
 };
